641a71cf4dc451bb266b65e9f33ce376628e27c7
Ayaalmadhon2004 <aya.alaasel123@gmail.com>
2026-07-06 18:45:54 +0200

    Finish theme and timer connection

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // الألوان اللي بتخلي التطبيق يبدو كأنه تطبيق احترافي (Dark Mode)
+  static const Color bgDark = Color(0xFF0F0F1A);      // خلفية التطبيق
+  static const Color accentPrimary = Color(0xFF6C5CE7); // لون الزر والهايلايت
+  static const Color textPrimary = Color(0xFFE0E0FF);  // لون النص
+}
